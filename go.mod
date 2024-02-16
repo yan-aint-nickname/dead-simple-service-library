67a8bc11b8305b188ec51a3fa0ec7c7c32d2ae95
Yan Kirsh <yankirsh@yandex.ru>
2024-02-16 10:26:13 +0300

    Start with something

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yan-aint-nickname/dead-simple-service-library
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func PrintHello(){
+	fmt.Println("Hello from dead simple service library")
+}
+
+func main() {}
